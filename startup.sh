51646306ec686fef90f05b86d7fd41f550e8fa0e
Administrator <admin@example.com>
2024-04-30 14:13:34 +0200

    Add auth for docker

--- startup.sh
+++ startup.sh
@@ -6,3 +6,6 @@ gcloud config set project ${GCP_PROJECT_NAME}
 
 # Activate the gcloud service account
 gcloud auth activate-service-account ${SERVICE_ACCOUNT_NAME}@${GCP_PROJECT_NAME}.iam.gserviceaccount.com --key-file=${SERVICE_ACCOUNT_PRIVATE_KEY_JSON}
+
+# Docker support
+gcloud auth configure-docker
